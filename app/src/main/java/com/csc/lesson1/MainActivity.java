3f390123b9d38318262c8064833602203b2e08f6
Vikulov Evgenii <jenyav94@gmail.com>
2016-02-17 23:59:18 +0300

    first commit

--- app/src/main/java/com/csc/lesson1/MainActivity.java
+++ app/src/main/java/com/csc/lesson1/MainActivity.java
@@ -2,11 +2,25 @@ package com.csc.lesson1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            Picasso.with(MainActivity.this)
+                    .load("https://pp.vk.me/c319717/v319717185/1e5b/jLNXWStC0j8.jpg")
+                    .into(imageView);
+
     }
 }
